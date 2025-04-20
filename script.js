6171dcf50c0fc8a778f548959164afce3b5508ce
PoornachanderReddy <poornachander295@gmail.com>
2025-04-20 23:49:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // write js code here if required
+function getTime(){
+	const time = new Date().toLocaleString();
+	document.getElementById('timer').innerHTML = time;
+}
+getTime();
+setIntervel(getTime,1000);
